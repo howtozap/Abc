dc79de439aa8bf36be52f54c65306784e7d71e90
rocky <rockysinghsgrl@gmail.com>
2022-04-04 17:28:42 +0530

    hi find new build

---
+++ src/main/java/Haramkhor/Mobile.java
@@ -0,0 +1,14 @@
+package Haramkhor;
+
+import org.testng.annotations.Test;
+
+public class Mobile {
+	@Test
+	public void test1(){
+		System.out.println("listning song");
+	}
+	public void test2() {
+		System.out.println("talking");
+	}
+
+}
